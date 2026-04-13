466aa27ad79cda8846a121926281160796b5d21a
Kushal Pawaskar <kushal.pawaskar@dream11.com>
2026-04-14 01:00:15 +0530

    update work gitconfig and nvim-diff for better mergetool

--- nvim_config/macos/work/vanilla/nvim-diff/init.lua
+++ nvim_config/macos/work/vanilla/nvim-diff/init.lua
@@ -9,3 +9,10 @@ vim.opt.runtimepath:append(main_config)
 -- Load shared config
 require("kpmac.core.options")
 require("kpmac.core.keymaps")
+
+-- Diff keymaps
+vim.keymap.set("n", "<leader>l", "<cmd>diffget LOCAL<cr>")
+vim.keymap.set("n", "<leader>r", "<cmd>diffget REMOTE<cr>")
+
+-- options
+vim.opt.timeoutlen = 200    -- default is 1000 ms
